3a237269b9802d35752939f44b520d0bfb480c65
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2021-12-22 22:45:36 +0700

    20190140053

---
+++ src/main/java/webservis/webservisc/WebserviscApplication.java
@@ -0,0 +1,13 @@
+package webservis.webservisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webservis/webservisc/WebserviscApplicationTests.java
@@ -0,0 +1,13 @@
+package webservis.webservisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
